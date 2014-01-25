14cd14249bc6a3df7b0c91925b7c308da36b22dc
David <dcjack57@hotmail.com>
2014-01-25 00:48:48 -0500

    tracking function added

--- php/track.php
+++ php/track.php
@@ -2,43 +2,22 @@
 class Track {
 	public function tracking($id){
 		//BEGIN - Open connection
-		$conn = mysql_connect('localhost', 'adminAxdwb5l', 'adI-YrkldDCK');
+		$conn = mysql_connect('localhost', 'adminL5a3gMp', '9sSyZmcTsEYD');
 		if (!$conn) {
 		    die('Could not connect: ' . mysql_error());
 		}
 		//END - Open connection
 
-		//BEGIN - Exists id?
-		$sql = "SELECT `count` FROM `usage` WHERE `id` = '" . $id . "'";
-		mysql_select_db('recinto');
-		$exists = mysql_query( $sql, $conn );
-		if(! $exists )
-		{
-		  die('Could not do select query: ' . mysql_error());
-		}
-		$counts = mysql_result($exists, 0);
-		//END - Exists id?
-
 		//BEGIN - Insert
-		if($exists == null){
-			$sql = "INSERT INTO `usage` (`id`,`count`) VALUES ('" . $id . "', 1)";
-			$retval = mysql_query( $sql, $conn );
-			if(! $retval )
-			{
-			  die('Could not enter data: ' . mysql_error());
-			}
-			//echo "Entered data successfully\n"; //END - Insert
-		} else { //BEGIN - Update
-			$counts = $counts + 1;
-			$sql = "UPDATE `usage` SET `count`= " . $counts . " WHERE `id` = '" . $id . "'";
-			$retval = mysql_query( $sql, $conn );
-			if(! $retval )
-			{
-			  die('Could not update row: ' . mysql_error());
-			}
-			//echo "Updated data successfully\n"; //END - Insert
+		mysql_select_db('elecciones2014');
+		$sql = "INSERT INTO `usage` (`id`,`date`) VALUES ('" . $id . "', NOW())";
+		$retval = mysql_query( $sql, $conn );
+		if(! $retval )
+		{
+			die('Could not enter data: ' . mysql_error());
 		}
-		//END - Update
+		//echo "Entered data successfully\n";
+		//END - Insert
 		
 		//BEGIN - Close connection
 		mysql_close($conn);
